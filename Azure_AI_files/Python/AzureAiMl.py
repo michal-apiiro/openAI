e942f77e6c04a60448ceb1faeec12497de010c14
Michal Feldman <michal@apiiro.com>
2023-10-18 15:25:01 +0300

    added examples

---
+++ Azure_AI_files/Python/AzureAiMl.py
@@ -0,0 +1,6 @@
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+
+ml_client = MLClient(
+    DefaultAzureCredential(), subscription_id, resource_group, workspace
+)
