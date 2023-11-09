731287d22a8bc98762169fc37d81728b08f0ea50
Michal Feldman <michal@apiiro.com>
2023-11-09 16:09:25 +0200

    add python example azure

--- Python/azure.py
+++ Python/azure.py
@@ -1,5 +1,14 @@
 from azure.openai import OpenAIClient 
 from azure.ai.ml import MLClient 
 
-new_client = OpenAIClient()
-new_client_azure = MLClient()
+
+def main():
+    model = "vicuna-7b-v1.5"
+    prompt = "Once upon a time"
+
+    new_client = OpenAIClient()
+    new_client_azure = MLClient()
+
+
+if __name__ == "__main__":
+    main()
