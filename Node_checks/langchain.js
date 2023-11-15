852c4b48d3265c38f2db235df8a23b26d1867435
Michal Feldman <michal@apiiro.com>
2023-11-15 15:33:19 +0200

    Examples for Node

--- Node_checks/langchain.js
+++ Node_checks/langchain.js
@@ -16,7 +16,6 @@ export const run = async () => {
     console.error(e);
     return;
   }
-
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
--- langchain/Python/example_llmOpenAI.py
+++
@@ -1,9 +0,0 @@
-from langchain.llms import AzureOpenAI
-# Create an instance of Azure OpenAI
-# Replace the deployment name with your own
-llm = AzureOpenAI(
-    deployment_name="td2",
-    model_name="text-davinci-002",
-)
-# Run the LLM
-llm("Tell me a joke")
