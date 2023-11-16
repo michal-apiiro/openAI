c3f1ebd378af8542232d0caa3ad58a3e65ff4c45
Michal Feldman <michal@apiiro.com>
2023-11-16 18:57:18 +0200

    anthropic

--- Node_new/anthropic2.ts
+++ Node_new/anthropic2.ts
@@ -1,11 +1,10 @@
 import Anthropic from '@anthropic-ai/sdk';
 
-const anthropic = new Anthropic({
-	apiKey: 'my api key', // defaults to process.env["ANTHROPIC_API_KEY"]
-});
-
 async function main() {
-  const completion = await anthropic.completions.create({
+    const anthropic = new Anthropic({
+        apiKey: 'my api key', // defaults to process.env["ANTHROPIC_API_KEY"]
+    });
+    const completion = await anthropic.completions.create({
     model: 'claude-2',
     max_tokens_to_sample: 300,
     prompt: `${Anthropic.HUMAN_PROMPT} How many toes do dogs have?${Anthropic.AI_PROMPT}`,
