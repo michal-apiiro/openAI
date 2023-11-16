0e62a907144a68089e27c5295c7ddce0c9f488aa
Michal Feldman <michal@apiiro.com>
2023-11-16 16:51:25 +0200

    Azure AI example

--- Node/huggingface.js
+++ Node/huggingface.js
@@ -1,3 +1,4 @@
 import { HfAgent } from "@huggingface/agents";
+
 const HF_ACCESS_TOKEN = "hf_..."; // get your token at https://huggingface.co/settings/tokens
 const agent = new HfAgent(HF_ACCESS_TOKEN);
---
+++ Node_2/azure.js
@@ -0,0 +1,5 @@
+const { OpenAIClient } = require("@azure/openai");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+const client = new OpenAIClient("<endpoint>", new DefaultAzureCredential());
+const openai = client
